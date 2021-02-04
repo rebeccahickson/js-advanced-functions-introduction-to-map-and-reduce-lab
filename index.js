3f2c8470be0042f9f8dd4b9803f7d299ea128fdc
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-04 16:31:04 -0500

    commit tree a1a49b832c8810f38dd041e8e0735ddfd15fca77

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+function mapToNegativize(arr) {
+  let newArr = [];
+  arr.forEach((element) => newArr.push(element * -1));
+  return newArr;
+}
+
+function mapToNoChange(arr) {
+  return arr;
+}
+
+function mapToDouble(arr) {
+  let newArr = [];
+  arr.forEach((element) => newArr.push(element * 2));
+  return newArr;
+}
+
+function mapToSquare(arr) {
+  let newArr = [];
+  arr.forEach((element) => newArr.push(element * element));
+  return newArr;
+}
+
+function reduceToTotal(arr, startNum = 0) {
+  let total = startNum;
+  arr.forEach((num) => (total += num));
+  return total;
+}
+
+function reduceToAllTrue(arr) {
+  let boolean = true;
+  arr.forEach((item) => {
+    if (!!item == false) {
+      boolean = false;
+      return boolean;
+    }
+  });
+
+  return boolean;
+}
+
+function reduceToAnyTrue(arr) {
+  let boolean = false;
+  arr.forEach((item) => {
+    if (!!item == true) {
+      boolean = true;
+      return boolean;
+    }
+  });
+  return boolean;
+}
